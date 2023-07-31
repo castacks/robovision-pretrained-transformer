90f60435c6c39c514cbdab064c37e27b34a7c507
Mihir Sharma <mihirsharma700@gmail.com>
2023-07-31 10:03:26 -0400

    added selective correlation function

--- unimatch/matching.py
+++ unimatch/matching.py
@@ -3,6 +3,40 @@ import torch.nn.functional as F
 
 from .geometry import coords_grid, generate_window_grid, normalize_coords
 
+def selective_correlation_softmax(feature0, feature1,
+                               pred_bidir_flow=False,
+                               ):
+    # selective correlation
+    b, c, h, w = feature0.shape
+    feature0 = feature0.view(b, c, -1).permute(0, 2, 1)  # [B, H*W, C]
+    feature1 = feature1.view(b, c, -1)  # [B, C, H*W]
+
+    feature0 = feature0
+    feature1 = feature1
+
+    correlation = torch.matmul(feature0, feature1).view(b, h, w, h, w) / (c ** 0.5)  # [B, H, W, H, W]
+
+    # flow from softmax
+    init_grid = coords_grid(b, h, w).to(correlation.device)  # [B, 2, H, W]
+    grid = init_grid.view(b, 2, -1).permute(0, 2, 1)  # [B, H*W, 2]
+
+    correlation = correlation.view(b, h * w, h * w)  # [B, H*W, H*W]
+
+    if pred_bidir_flow:
+        correlation = torch.cat((correlation, correlation.permute(0, 2, 1)), dim=0)  # [2*B, H*W, H*W]
+        init_grid = init_grid.repeat(2, 1, 1, 1)  # [2*B, 2, H, W]
+        grid = grid.repeat(2, 1, 1)  # [2*B, H*W, 2]
+        b = b * 2
+
+    prob = F.softmax(correlation, dim=-1)  # [B, H*W, H*W]
+
+    correspondence = torch.matmul(prob, grid).view(b, h, w, 2).permute(0, 3, 1, 2)  # [B, 2, H, W]
+
+    # when predicting bidirectional flow, flow is the concatenation of forward flow and backward flow
+    flow = correspondence - init_grid
+
+    return flow, prob
+
 
 def global_correlation_softmax(feature0, feature1,
                                pred_bidir_flow=False,
