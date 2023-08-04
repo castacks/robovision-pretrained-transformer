2721e3c57b26cb0de7fa3645738588b1218eecd9
Mihir Sharma <mihirsharma700@gmail.com>
2023-08-04 15:00:18 -0400

    added decompression function

--- dataloader/matching/datasets.py
+++ dataloader/matching/datasets.py
@@ -1,5 +1,6 @@
 import torch
 import sys
+import numpy as np
 import tartanair as ta
 from torch.utils.data import Dataset, DataLoader
 
@@ -14,3 +15,18 @@ def build_train_dataset():
                                                         camera_name = ['lcam_front', 'lcam_back', 'lcam_right', 'lcam_left', 'lcam_top', 'lcam_bottom'], 
                                                         transform = None, num_workers=10) #FIXME
     return matching_dataset
+
+
+# uncompress the data
+def flow16to32(flow16):
+    '''
+    flow_32b (float32) [-512.0, 511.984375]
+    flow_16b (uint16) [0 - 65535]
+    flow_32b = (flow16 -32768) / 64
+    '''
+    flow32 = flow16[:,:,:2].astype(np.float32)
+    flow32 = (flow32 - 32768) / 64.0
+
+    mask8 = flow16[:,:,2].astype(np.uint8)
+    return flow32, mask8
+
